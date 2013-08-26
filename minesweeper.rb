590f8dc8f2c6ff19af36b30fa3b41d58494873ca
Colin Mackenzie and Anna Kopp <colinwmackenzie@gmail.com koppanna@gmail.com>
2013-08-26 11:39:10 -0400

    Board randomizes bombs and prints itself

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,6 +1,8 @@
+load 'square.rb'
+
 class Minesweeper
 
-  attr_accessor :size, :bomb_num
+  attr_accessor :size, :bomb_num, :board
 
   def initialize(size, bomb_num)
     @size = size
@@ -11,8 +13,9 @@ class Minesweeper
   def build_board
     @size.times do |x|
       @board[x] = []
-      x.each do |y|
-        square = Square.new(x, y)
+
+      @size.times do |y|
+        square = Square.new(x, y, false)
         @board[x][y] = square
       end
     end
@@ -20,17 +23,51 @@ class Minesweeper
 
   def fill_bombs
     placed_bombs = 0
-    coordinates = (1..@size).to_a
+    coordinates = (0...@size).to_a
 
-    until placed_boms = @bomb_num
-      x = coordinates.shuffle
-      y = coordinates.shuffle
-
+    until placed_bombs == @bomb_num
+      x = coordinates.sample
+      y = coordinates.sample
       unless @board[x][y].bomb
+
        @board[x][y].bomb = true
        placed_bombs += 1
       end
     end
   end
 
+  def print_board
+    @board.each do |x|
+      x.each { |y| print y.display + " " }
+      puts ""
+    end
+  end
+
+  def run
+    build_board
+    fill_bombs
+    print_board
+
+    until @flag_num == @bomb_num
+      user_input
+    end
+
+    check_victory #will
+
+  end
+
+  def user_input
+    p "Do something"
+    input = gets.chomp
+    case input[0]
+    when "r"
+      reveal(input[1], input[2])
+    when "f"
+      flag(input[1], input[2])
+    end
+  end
+
 end
+
+ms = Minesweeper.new(9, 10)
+
--- square.rb
+++ square.rb
@@ -1,14 +1,11 @@
 class Square
-  attr_accessor :bomb
+  attr_accessor :bomb, :display
 
   def initialize(x, y, bomb = false, display = "*")
     @x = x
     @y = y
-
     @bomb = bomb
     @display = display
   end
-
-
 
 end
